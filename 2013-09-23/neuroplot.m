cd7b3e19af7b646c6d25050092f0f2b65b055b5a
ecarmichael <ecarmichael@gmail.com>
2013-09-25 13:11:08 -0400

    Neuroplot (Eric) with evts
    The events have been added as well as a "default_zoom' variable.

--- 2013-09-23/neuroplot.m
+++ 2013-09-23/neuroplot.m
@@ -15,15 +15,18 @@ function neuroplot(spikes,csc,varargin)
 % evtColor: [nEvents x 3] RGB values to plot events, default []
 %
 % interactiveMode: boolean to enable/disable arrow key navigation
+%
+%default_zoom:  [nCells x 2]  This determines the section that will be
+%magnified in the plot.  By default it is set to 4000 - 4002s.  
 
 %% Initilize default values
 cscColor = [.7 .7 .7];
 spikeColor = [1 0 0];
+evtColor = [0 0 0];
+evt = 0:10:max(Data(spikes{1}));
 interactiveMode = 1; % for the interactive figure navigator 1 is on and 0 is off.  
-evt = 4000;
+default_zoom = [4000 4002];
 extract_varargin;  % this will allow for modifications to the default values
-
-
 
 %% extract the spike times from the S cell of ts values
 spike_times = cell(1,length(spikes));
@@ -59,10 +62,14 @@ data_mod = data./500 +(length(spike_times)+5);
 csc_plot = plot(time,data_mod);
 set(csc_plot,'Color', cscColor)
 
+%% Add the event lines
+for ev = 1:length(evt)
+    line(evt(ev),0:0.1:80,'Color',evtColor)
+end
 
 %% Make some restrictions and alter the figure.
 set(gcf,'Color',[1 1 1])
-set(gca,'Visible','off','XLim', [evt-1 evt+1]) %takes the specified event time and plots 1s on either side
+set(gca,'Visible','off','XLim', default_zoom) %takes the specified event time and plots 1s on either side
 
 % set(gca,'YTick',[1:length(spike_times{ss})])
 
